26c4a3ab96698600a815007f59c3b33ecd8f654f
fwas <fwas.corp@gmail.com>
2024-07-14 19:24:37 +0200

    first post schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { postType } from './schemaTypes/postType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [postType],
 }
---
+++ sanity/schemaTypes/postType.ts
@@ -0,0 +1,38 @@
+import { defineField, defineType } from "sanity";
+
+export const postType = defineType({
+  name: "post",
+  title: "Blog post",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      type: "string",
+      validation: (rule) => rule.required().min(10).max(80),
+    }),
+    defineField({
+      name: "slug",
+      type: "slug",
+      options: { source: "title" },
+      validation: (rule) =>
+        rule.required().error("Required to generate a page on the website."),
+      hidden: ({ document }) => !document?.title,
+    }),
+    {
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
+      validation: (rule) => rule.required().error("Required to publish the post."),
+    },
+    defineField({
+      name: "image",
+      type: "image",
+    }),
+    defineField({
+      name: "body",
+      title: "Post body",
+      type: "array",
+      of: [{ type: "block" }],
+    }),
+  ],
+});
