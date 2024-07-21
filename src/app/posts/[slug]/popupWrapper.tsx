3f46ffb1b9c1a8a5ec0c8c3e3c1be90a6bfca8cb
fwas <fwas.corp@gmail.com>
2024-07-21 20:44:24 +0200

    main page image works as link + minor improvements

--- src/app/posts/[slug]/popupWrapper.tsx
+++ src/app/posts/[slug]/popupWrapper.tsx
@@ -1,17 +1,33 @@
 'use client'
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import LeadCollector from './leadCollector';
+import useLocalStorage from '@/hooks/useLocalStorage';
 
 interface LeadCollectorProps {
 }
 
 const PopupWrapper: React.FC<LeadCollectorProps> = () => {
   const [email, setEmail] = useState<string>('');
-  const [isOpen, setIsOpen] = useState(true)
+  const [isOpen, setIsOpen] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [popupShown, setPopupShown] = useLocalStorage('popupShown', 'false');
+
+  useEffect(() => {
+    if (popupShown === 'false') {
+      setIsOpen(true);
+      setPopupShown('true');
+    }
+  }, [popupShown, setPopupShown]);
+
+  const closePopup = () => {
+    setIsOpen(false);
+  };
+
+  if (!isOpen) return null;
 
   return (
-    <LeadCollector email={email} setEmail={setEmail} isOpen={isOpen} setIsOpen={setIsOpen}/>
+    <LeadCollector email={email} setEmail={setEmail} closePopup={closePopup} isLoading={isLoading} setIsLoading={setIsLoading}/>
   );
 };
 
