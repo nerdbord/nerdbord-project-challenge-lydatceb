01a3b782f64d2de04dc97d355663337e1ca16300
fwas <fwas.corp@gmail.com>
2024-07-22 19:14:43 +0200

    cron job endpoint secured

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,8 +1,17 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { createPost } from '../../../scripts/createPost.mjs'
 
+const cronSecretToken = process.env.CRON_ENDPOINT_TOKEN
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
+        
+        const {token} = req.query;
+
+        if (!token || token !== cronSecretToken) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
          const today = new Date().getDay()
          if (today === 0 || today === 6) {
              //don't generate posts on weekend
